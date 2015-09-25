65f4a5c808d60d896ebef829553f23d2889d9bcb
Peter Foot <peter@peterfoot.net>
2015-09-25 23:15:53 +0100

    demo

--- MapsTest/MapsTest/MapPage.xaml.cs
+++ MapsTest/MapsTest/MapPage.xaml.cs
@@ -14,15 +14,15 @@ namespace MapsTest
         {
             InitializeComponent();
 
-            MyMap.Pins.Add(new Xamarin.Forms.Maps.Pin() { Label = "Cheese", Position = new Xamarin.Forms.Maps.Position(52.7228617, -2.4163171) });
-            MyMap.MoveToRegion(Xamarin.Forms.Maps.MapSpan.FromCenterAndRadius(new Xamarin.Forms.Maps.Position(52.7228617, -2.4163171), Xamarin.Forms.Maps.Distance.FromKilometers(3)));
+            MyMap.Pins.Add(new Xamarin.Forms.Maps.Pin() { Label = "Cheese", Position = new Xamarin.Forms.Maps.Position(52.4817055, -1.9065627) });
+            MyMap.MoveToRegion(Xamarin.Forms.Maps.MapSpan.FromCenterAndRadius(new Xamarin.Forms.Maps.Position(52.4817055, -1.9065627), Xamarin.Forms.Maps.Distance.FromKilometers(3)));
 
             Task.Run(async () =>
             {
                 await Task.Delay(10000);
                 Device.BeginInvokeOnMainThread(() =>
                 {
-                    MyMap.Pins.Add(new Xamarin.Forms.Maps.Pin() { Label = "Toast", Position = new Xamarin.Forms.Maps.Position(52.7586941, -2.3542056) });
+                    MyMap.Pins.Add(new Xamarin.Forms.Maps.Pin() { Label = "Toast", Position = new Xamarin.Forms.Maps.Position(52.479014, -1.9039143) });
                 });
 
             });
